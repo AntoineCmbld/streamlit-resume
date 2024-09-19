4955c193a1c086a915d16c2276fd1ac345f598c9
Antoine <loliwithautism@gmail.com>
2024-09-19 23:23:59 +0200

    translate edu

--- education.py
+++ education.py
@@ -6,23 +6,23 @@ def education_page():
         st.write(
             """
             **EFREI Paris** | 2021-2026
-            - École d'ingénieur informatique en 5 ans. Actuellement en deuxième année de cycle ingénieur (M1).
-            - Cours suivis: Advanced databases, Data visualization, Machine learning, API and web services, and Maths for data scientists
+            - 5-year computer engineering school. Currently in the second year of the engineering cycle (M1).
+            - Courses taken: Advanced databases, Data visualization, Machine learning, API and web services, and Maths for data scientists
             """
         )
     with st.container(border=True):
         st.write(
-            """
+            '''
             **APU Kuala Lumpur** | 2023
-            - Semestre d’étude en Malaisie
-            - Cours suivis: Advanced web programming, Java programming, Object-oriented Analysis and Design with UML
-            """
+            - Study semester in Malaysia
+            - Courses taken: Advanced web programming, Java programming, Object-oriented Analysis and Design with UML
+            '''
         )
     with st.container(border=True):
         st.write(
             """
             **Saint-Jean Hulst** | 2006-2021
-            - Bac spécialité NSI/Physique avec mention Bien
+            - High school diploma with a focus on computer science and physics with a grade of Bien
             """
         )
     
